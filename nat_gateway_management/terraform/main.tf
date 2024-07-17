25b806d9cea3a63a78e77638359804ba72ac633f
Ian Forrest <ianforrest@Ians-MacBook-Air.local>
2024-07-16 23:40:52 -0600

    test

--- nat_gateway_management/terraform/main.tf
+++ nat_gateway_management/terraform/main.tf
@@ -1,6 +1,6 @@
 module "default_vpc_nat_gateway_public_subnet_1" {
   source                    = "git@github.com:ianforrest11/terraform_templates.git//aws/vpc_nat_gateway?ref=main"
-  allocation_id             = data.aws_eip.default_vpc_nat_gateway_public_subnet_2.id
+  allocation_id             = data.aws_eip.default_vpc_nat_gateway_public_subnet_1.id
   name                      = var.default_vpc_nat_gateway_public_subnet_1_name
   subnet_id                 = data.aws_subnet.default_vpc_public_subnet_1.id
 }
