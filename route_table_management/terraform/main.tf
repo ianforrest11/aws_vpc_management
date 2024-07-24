339a2b1509de20bd38130e70874f1ab628dca863
Ian Forrest <ianforrest@Ians-MacBook-Air.local>
2024-07-24 15:24:47 -0600

    remove common steps, first apply test

--- route_table_management/terraform/main.tf
+++ route_table_management/terraform/main.tf
@@ -1,6 +1,6 @@
 module "default_vpc_public_route_table_1" {
   source                    = "git@github.com:ianforrest11/terraform_templates.git//aws/vpc_route_table?ref=main"
-  name                      = var.default_vpc_public_route_table_name
+  name                      = var.default_vpc_public_route_table_1_name
   vpc_id                    = data.aws_vpc.default_vpc.id
 }
 
