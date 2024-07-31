0e70ee6de9060a638b5c9fa8248e392f2ae965fe
Ian Forrest <ianforrest@Ians-MacBook-Air.local>
2024-07-30 20:59:20 -0600

    adding dns hostnames to vpc

--- vpc_management/terraform/main.tf
+++ vpc_management/terraform/main.tf
@@ -1,7 +1,7 @@
 module "default_vpc" {
   source                = "git@github.com:ianforrest11/terraform_templates.git//aws/vpc_vpc?ref=main"
   cidr_block            = var.main_vpc_cidr_block
-  enable_dns_hostnames  = var.enable_dns_hostnames
+  enable_dns_hostnames  = var.main_vpc_enable_dns_hostnames
   instance_tenancy      = var.main_vpc_instance_tenancy
   name                  = var.main_vpc_name
 }
